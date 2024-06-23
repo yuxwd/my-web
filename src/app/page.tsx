c41c6379ad6f519df9678db44607159110732f06
yuxwd <yuxiang.ji@outlook.com>
2024-06-23 09:08:36 +0800

    OK!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -223,7 +223,7 @@ function Resume() {
     <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Work</span>
+        <span className="ml-3">工作</span>
       </h2>
       <ol className="mt-6 space-y-4">
         {resume.map((role, roleIndex) => (
@@ -231,7 +231,7 @@ function Resume() {
         ))}
       </ol>
       <Button href="#" variant="secondary" className="group mt-6 w-full">
-        Download CV
+        下载
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -132,11 +132,11 @@ function MobileNavigation(
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                <MobileNavItem href="/about">About</MobileNavItem>
-                <MobileNavItem href="/articles">Articles</MobileNavItem>
-                <MobileNavItem href="/projects">Projects</MobileNavItem>
-                <MobileNavItem href="/speaking">Speaking</MobileNavItem>
-                <MobileNavItem href="/uses">Uses</MobileNavItem>
+                <MobileNavItem href="/about">关于</MobileNavItem>
+                <MobileNavItem href="/articles">文章</MobileNavItem>
+                <MobileNavItem href="/projects">项目</MobileNavItem>
+                <MobileNavItem href="/speaking">演说</MobileNavItem>
+                <MobileNavItem href="/uses">毫无关系</MobileNavItem>
               </ul>
             </nav>
           </Popover.Panel>
